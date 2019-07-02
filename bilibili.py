70414ad6ed7b9f477bd635d8ef1e4cb815ef40a5
Richard-D-SUN <2407430403@qq.com>
2019-07-02 17:58:10 +0800

    ip测试完成，接下来解决速率问题

--- bilibili.py
+++ bilibili.py
@@ -1,13 +1,26 @@
 import requests
 import json
 import base64
+import pymongo
 import time
 from multiprocessing import Pool
 
 global total
 total = 1
 
+#查最大值：db.person.find({}).sort({"num" : -1}).limit(1)
 
+
+def get_proxies_2():
+    url = "https://proxy.horocn.com/api/proxies?order_id=5XPC1637821784969771&num=1&format=json&line_separator=win&can_repeat=no"
+    jsontext = json.loads(requests.get(url).text)
+    for temp in jsontext:
+        host = temp['host']
+        port = temp['port']
+    proxies = {}
+    proxies["http"] = "http://" + host + ":" + port
+    proxies["https"] = "https://" + host + ":" + port
+    return proxies
 
 def get_proxies():
     strr = "G3KV1637823384560299:qIcYzrkKaiESbkpV"
@@ -26,10 +39,13 @@ def get_proxies():
     }
     return proxies
 
-def spider(ran):
+def spider(ran,mycol):
     print("kaishi")
-    flag = False
-    proxies = ""
+    flag = True
+    proxies = {
+        "http": "http://115.226.145.205:28437",
+        "https": "https://115.226.145.205:28437",
+    }
     for i in range(ran[0],ran[1]):
         while(True):
             url = 'https://api.bilibili.com/x/web-interface/card?mid=' + str(i) + '&photo=true'
@@ -37,63 +53,54 @@ def spider(ran):
             try:
                 while (True):
                     if (flag == False):
-                        proxies = {
-                            "http":"http://183.166.6.242:26577",
-                            "https":"https://183.166.6.242:26577",
-                    }
-                    print(proxies)
-                    data = requests.get(url=url,proxies=proxies,timeout=5)
-                    print("有问题")
+                        proxies = get_proxies_2()
+                        print('更换代理')
+                        print(proxies)
+                    data = requests.get(url=url,proxies=proxies,timeout=10)
                     if (data.status_code == 200):
                         flag = True
-                        print("有效的代理")
                         break
                     else:
                         flag = False
-                        print(data.status_code)
-                        tt = requests.get(url="http://httpbin.org/get", proxies=proxies)
-                        print(tt.text)
                 break
-            except:
+            except Exception as e:
+                flag = False
+                print(e)
                 print('继续')
         if (data.status_code == 200):
             result = data.text
             js = json.loads(result)
             if (js['code'] == 0):
+                mydict = {}
                 fannum = js['data']['card']['fans']
                 Name = js['data']['card']['name']
-                number = js['data']['card']['mid'];
+                number = js['data']['card']['mid']
+                mydict['fannum'] = fannum
+                mydict['name'] = Name
+                mydict['num'] = int(number)
                 print("编号: " + str(number) + "昵称：" + str(Name) + "粉丝数量：" + str(fannum))
+                mycol.insert_one(mydict)
     return
 
 
 if __name__ == '__main__':
-    #spider([1,2000])
-    # start_time = time.time()
-    # pool = Pool(processes=4)
-    # canshu1 = [1,500]
-    # canshu2 = [501,1000]
-    # canshu3 = [1001,1500]
-    # canshu4 = [1501,2000]
-    # pool.map(spider,zip(canshu1,canshu2,canshu3,canshu4))
-    # end_time = time.time()
-    # print("耗时:",end_time-start_time)
-    #proxy = requests.get("https://proxy.horocn.com/api/proxies?order_id=5XPC1637821784969771&num=10&format=json&line_separator=win&can_repeat=no")
-    #js = json.loads(proxy.text)
-
-    # for temp in js:
-    #     prox = "https://" + temp['host'] + ":" + temp['port']
-    #     prox2 = "http://" + temp['host'] + ":" + temp['port']
-    #     proxies = {}
-    #     proxies["https:"] = prox
-    #     proxies["http:"] = prox2
-    #     print(proxies)
-    # proxies = get_proxies()
-    proxies = {
-        "http":"http://180.122.144.195:45689",
-        "https":"https://180.122.144.195:45689",
-    }
-    result = requests.get(url="http://httpbin.org/get",proxies=proxies,timeout=1)
-    print(result.text)
+    print(int("154784646"))
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient['bilibili-fans']
+    mycol = mydb['sites']
+    record = mycol.find().sort("num",-1).limit(1)
+    if (record.count() != 0):
+        for temp in record:
+            print("数据库中的最大值为:"+str(temp['num']))
+            start = temp['num'] + 1
+    else:
+        start = 1
+    spider([start,5000000000],mycol)
+    # proxies = {
+    #     "http":"http://59.33.136.130:28526",
+    #     "https":"https://59.33.136.130:28526",
+    # }
+    # result = requests.get(url="http://httpbin.org/get",proxies=proxies,timeout=3)
+    # print(result.text)
 
 
